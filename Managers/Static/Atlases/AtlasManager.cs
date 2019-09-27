fc2b5ab9d5bced421f085fc4efa1c06163b02354
Ordmund <main.cableguy@gmail.com>
2019-09-27 21:41:27 +0300

    IDistributable empty implementation in atlas manager.

--- Managers/Static/Atlases/AtlasManager.cs
+++ Managers/Static/Atlases/AtlasManager.cs
@@ -1,7 +1,8 @@
 ﻿using System;
+using Core.Managers;
 using UnityEngine;
 
-public static class AtlasManager
+public class AtlasManager : IDistributable //TODO ????????
 {
     public static Sprite GetSprite(string spriteName)
     {
@@ -11,5 +12,21 @@ public static class AtlasManager
     private static void LoadAtlas()
     {
 
+    }
+
+    public int InitializationGeneration { get; }
+    public void Initialize(Distributor distributor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Restart(Distributor distributor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
     }
 }

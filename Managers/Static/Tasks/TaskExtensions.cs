d4efd1c2be056b0af4058bf7716180c6b841277c
Ordmund <vladyslav.alvgeir@gmail.com>
2025-09-18 23:21:27 +0900

    Moved  Task events subscription and calling to AsyncTask.

--- Managers/Static/Tasks/TaskExtensions.cs
+++ Managers/Static/Tasks/TaskExtensions.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Threading.Tasks;
+using Core.Tasks;
 using UnityEngine;
 
 namespace Core.Managers
@@ -9,79 +9,65 @@ namespace Core.Managers
         /// <summary>
         /// Runs the specified task and handles any exceptions that occur during the process
         /// </summary>
-        /// <param name="task">A task that needs to be started from synchronous code</param>
-        public static async void Forget(this Task task)
+        /// <param name="asyncTask">A task that needs to be started from synchronous code</param>
+        public static async void RunAndForget(this AsyncTask asyncTask)
         {
             try
             {
+                var task = asyncTask.Execute();
+
                 await task;
+
+                if (task.IsCompletedSuccessfully)
+                {
+                    asyncTask.InvokeOnCompleted();
+                }
+
+                if (task.IsCanceled)
+                {
+                    asyncTask.InvokeOnCanceled();
+
+                    Debug.Log($"{asyncTask.GetType().Name} is canceled");
+                }
             }
             catch (Exception error)
             {
-                Debug.LogError(error.Message);
+                asyncTask.InvokeOnFaulted();
+
+                Debug.LogError($"{asyncTask.GetType().Name} is failed with the exception: {error.Message}");
             }
         }
 
         /// <summary>
         /// Runs the specified task and handles any exceptions that occur during the process
         /// </summary>
-        /// <param name="task">A task that needs to be started from synchronous code</param>
+        /// <param name="asyncTask">A task that needs to be started from synchronous code</param>
         /// <typeparam name="T">The type of result of successful task completion</typeparam>
-        public static async void Forget<T>(this Task<T> task)
+        public static async void RunAndForget<T>(this AsyncTask<T> asyncTask)
         {
             try
             {
+                var task = asyncTask.Execute();
+
                 await task;
+
+                if (task.IsCompletedSuccessfully)
+                {
+                    asyncTask.InvokeOnCompleted();
+                }
+
+                if (task.IsCanceled)
+                {
+                    asyncTask.InvokeOnCanceled();
+
+                    Debug.Log($"{asyncTask.GetType().Name} is canceled");
+                }
             }
             catch (Exception error)
             {
-                Debug.LogError(error.Message);
-            }
-        }
+                asyncTask.InvokeOnFaulted();
 
-        /// <summary>
-        /// Call the specified action if the task is completed Successfully
-        /// </summary>
-        /// <param name="task">A task that needs to be started from the main thread</param>
-        /// <param name="onComplete">Action to call on task completed</param>
-        /// <returns>Task with subscription</returns>
-        /// <remarks>
-        /// ⚠️ WARNING: This method must be called only from main thread, not from asynchronous code
-        /// </remarks>
-
-        public static Task OnComplete(this Task task, Action onComplete)
-        {
-            return task.ContinueWith(CallOnComplete, TaskScheduler.FromCurrentSynchronizationContext());
-
-            void CallOnComplete(Task completedTask)
-            {
-                if(completedTask.IsCompletedSuccessfully)
-                {
-                    onComplete.Invoke();
-                }
-            }
-        }
-        
-        /// <summary>
-        /// Call the specified action if the task is canceled
-        /// </summary>
-        /// <param name="task">A task that needs to be started from the main thread</param>
-        /// <param name="onCanceled">Action to call on task canceled</param>
-        /// <returns>Task with subscription</returns>
-        /// <remarks>
-        /// ⚠️ WARNING: This method must be called only from main thread, not from asynchronous code
-        /// </remarks>
-
-        public static Task OnCanceled(this Task task, Action onCanceled)
-        {
-            return task.ContinueWith(CallOnAbort, TaskScheduler.FromCurrentSynchronizationContext());
-            
-            void CallOnAbort(Task cancelledTask)
-            {
-                if(cancelledTask.IsCanceled)
-                {
-                    onCanceled.Invoke();
-                }
+                Debug.LogError($"{asyncTask.GetType().Name} is failed with the exception: {error.Message}");
             }
         }
     }
--- Tasks/AsyncTask.cs
+++ Tasks/AsyncTask.cs
@@ -1,14 +1,95 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Core.Tasks
 {
     public abstract class AsyncTask
     {
+        private event Action OnCompleted;
+        private event Action OnCanceled;
+        private event Action OnFaulted;
+        
         public abstract Task Execute();
+
+        public AsyncTask OnComplete(Action action)
+        {
+            OnCompleted += action;
+
+            return this;
+        }
+        
+        public AsyncTask OnCancel(Action action)
+        {
+            OnCanceled += action;
+
+            return this;
+        }
+        
+        public AsyncTask OnFault(Action action)
+        {
+            OnFaulted += action;
+
+            return this;
+        }
+
+        public void InvokeOnCompleted()
+        {
+            OnCompleted?.Invoke();
+        }
+        
+        public void InvokeOnCanceled()
+        {
+            OnCanceled?.Invoke();
+        }
+        
+        public void InvokeOnFaulted()
+        {
+            OnFaulted?.Invoke();
+        }
     }
     
     public abstract class AsyncTask<T>
     {
+        private event Action OnCompleted;
+        private event Action OnCanceled;
+        private event Action OnFaulted;
+        
         public abstract Task<T> Execute();
+
+        public AsyncTask<T> OnComplete(Action action)
+        {
+            OnCompleted += action;
+
+            return this;
+        }
+        
+        public AsyncTask<T> OnCancel(Action action)
+        {
+            OnCanceled += action;
+
+            return this;
+        }
+        
+        public AsyncTask<T> OnFault(Action action)
+        {
+            OnFaulted += action;
+
+            return this;
+        }
+
+        public void InvokeOnCompleted()
+        {
+            OnCompleted?.Invoke();
+        }
+        
+        public void InvokeOnCanceled()
+        {
+            OnCanceled?.Invoke();
+        }
+        
+        public void InvokeOnFaulted()
+        {
+            OnFaulted?.Invoke();
+        }
     }
 }

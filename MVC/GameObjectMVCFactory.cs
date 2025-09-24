f05d3c2bd4ebec6cd1213a763b8f50bad7aeb39b
Ordmund <vladyslav.alvgeir@gmail.com>
2025-09-24 13:28:06 +0900

    Fixed IInitializable Initialize() not being called when an instance created via factory.

--- MVC/GameObjectMVCFactory.cs
+++ MVC/GameObjectMVCFactory.cs
@@ -9,49 +9,62 @@ namespace Core.MVC
     {
         private readonly DiContainer _container;
         private readonly IPrefabsPathProvider _prefabsPathProvider;
-        
+
         public GameObjectMVCFactory(DiContainer container, IPrefabsPathProvider prefabsPathProvider)
         {
             _container = container;
             _prefabsPathProvider = prefabsPathProvider;
         }
-        
+
         public async Task<TController> InstantiateAndBindAsync<TController, TView, TModel>(string path = null)
-            where TController : BaseController<TView, TModel> 
+            where TController : BaseController<TView, TModel>
             where TView : BaseView
             where TModel : BaseModel
         {
             path ??= _prefabsPathProvider.GetPathByViewType<TView>();
             var asyncOperationHandle = Addressables.InstantiateAsync(path);
             var gameObject = await asyncOperationHandle.Task;
+            
             var view = gameObject.GetComponent<TView>();
-            var model = GetModel<TModel>();
+            var model = CreateModel<TModel>();
             var controller = BindAndResolve<TController, TView, TModel>(view, model);
+            
+            TryCallInitialize(controller);
 
             return controller;
         }
 
         public TController FindObjectAndBind<TController, TView, TModel>()
-            where TController : BaseController<TView, TModel> 
+            where TController : BaseController<TView, TModel>
             where TView : BaseView
             where TModel : BaseModel
         {
             var view = UnityEngine.Object.FindAnyObjectByType<TView>();
-            var model = GetModel<TModel>();
+            var model = CreateModel<TModel>();
             var controller = BindAndResolve<TController, TView, TModel>(view, model);
+            
+            TryCallInitialize(controller);
 
             return controller;
         }
 
         private TController BindAndResolve<TController, TView, TModel>(TView view, TModel model)
         {
-            _container.Bind<TController>().AsSingle().WithArguments(view, model).NonLazy();
+            _container.BindInterfacesAndSelfTo<TController>().AsSingle().WithArguments(view, model).NonLazy();
             var controller = _container.Resolve<TController>();
 
             return controller;
         }
 
-        private static TModel GetModel<TModel>() where TModel : BaseModel
+        private static void TryCallInitialize<TController>(TController controller)
+        {
+            if (controller is IInitializable initializable)
+            {
+                initializable.Initialize();
+            }
+        }
+
+        private static TModel CreateModel<TModel>() where TModel : BaseModel
         {
             return Activator.CreateInstance<TModel>();   
         }

5c9b35979c7266889b19f0c641d678fbf0ca4209
Ordmund <vladyslav.alvgeir@gmail.com>
2025-09-24 14:32:25 +0900

    Added ModelLoader interface for scriptable objects.

--- MVC/Components/BaseController.cs
+++ MVC/Components/BaseController.cs
@@ -1,14 +1,24 @@
+using Core.Managers.ScriptableObjects;
+using UnityEngine;
+
 namespace Core.MVC
 {
     public abstract class BaseController<TView, TModel>
     {
         protected TView View { get; }
-        protected TModel Model { get; }
+        protected TModel Model { get; private set; }
 
         protected BaseController(TView view, TModel model)
         {
             View = view;
             Model = model;
+        }
+
+        protected void TryLoadModelFromScriptableObject<TScriptableObject>() where TScriptableObject : ScriptableObject, IModelLoader<TModel>
+        {
+            IModelLoader<TModel> modelLoader = ScriptableObjectsManager.Load<TScriptableObject>();
+
+            Model = modelLoader.LoadModel();
         }
     }
 }
---
+++ MVC/Components/IModelLoader.cs
@@ -0,0 +1,7 @@
+namespace Core.MVC
+{
+    public interface IModelLoader<out TModel>
+    {
+        public TModel LoadModel();
+    }
+}
